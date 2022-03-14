5b54b288c97c6e4487754175a00afb1a7f88028c
Koblova <alina.kirov@mail.ru>
2022-03-15 00:18:33 +0300

    fix media 320

--- js/main.js
+++ js/main.js
@@ -1,99 +1,170 @@
 $(document).ready(function () {
+  //видеоплеер
   var playerbig;
   $(".video-play").on("click", function onYouTubeIframeAPIReady() {
     player0 = new YT.Player("playerbig", {
       height: "100%",
       width: "100%",
       videoId: "DSG28w5Hr5U",
+      host: 'https://www.youtube.com',
       events: {
-        
+
       },
     });
   });
 
   var playersmall1;
-  $(".small-button1").on("click", function onYouTubeIframeAPIReady() {
+  $(".small-button--1").on("click", function onYouTubeIframeAPIReady() {
     player0 = new YT.Player("playersmall1", {
       height: "100%",
       width: "100%",
       videoId: "DSG28w5Hr5U",
+      host: 'https://www.youtube.com',
       events: {
-        
-      },
+
+      }
     });
   });
 
   var playersmall2;
-  $(".small-button2").on("click", function onYouTubeIframeAPIReady() {
+  $(".small-button--2").on("click", function onYouTubeIframeAPIReady() {
     player0 = new YT.Player("playersmall2", {
       height: "100%",
       width: "100%",
       videoId: "DSG28w5Hr5U",
+      host: 'https://www.youtube.com',
       events: {
-        
-      },
+
+      }
     });
   });
 
   var playersmall3;
-  $(".small-button3").on("click", function onYouTubeIframeAPIReady() {
+  $(".small-button--3").on("click", function onYouTubeIframeAPIReady() {
     player0 = new YT.Player("playersmall3", {
       height: "100%",
       width: "100%",
       videoId: "DSG28w5Hr5U",
+      host: 'https://www.youtube.com',
       events: {
-        onReady: videoPlay,
-      },
+
+      }
     });
   });
-/*
-  var likeS = document.getElementById("mark2");
-  likeS.addEventListener("click", function () {
-    document.getElementById("mark2").classList.toggle("bookmark--active");
-  });
 
-  var likeS = document.getElementById("mark3");
-  likeS.addEventListener("click", function () {
-    document.getElementById("mark3").classList.toggle("bookmark--active");
-  });
+//скролл
+  const anchors = document.querySelectorAll('a[href^="#"]')
+
+  for(let anchor of anchors) {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault()
+    const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body'
+    document.querySelector(goto).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    })
+  })
+}
+
+/*
+//скролл нерабочий
+  function scrollTo(element) {
+    window.scroll({
+      left: 0,
+      top: element.offsetTop,
+      behavior: "smooth",
+    })
+  }
+
+  var href = document.querySelector(".navbar-menu__link--1");
+  var unreleased = document.querySelector("#unreleased");
+
+  href.addEventListener("click", () => {
+    scrollTo(unreleased);
+  })
+
+  var href = document.querySelector(".navbar-menu__link--2");
+  var book = document.querySelector("#book");
+
+  href.addEventListener("click", () => {
+    scrollTo(book);
+  })
+
+  var href = document.querySelector(".navbar-menu__link--3");
+  var blog = document.querySelector("#blog");
+
+  href.addEventListener("click", () => {
+    scrollTo(blog);
+  })
 */
-  var likeF = document.getElementById("like1");
-  likeF.addEventListener("click", function () {
+
+  //эффект нажатия
+  var like = document.getElementById("like1");
+  like.addEventListener("click", function () {
     document.getElementById("like1").classList.toggle("like--active");
   });
 
-  var likeF = document.getElementById("like2");
-  likeF.addEventListener("click", function () {
+  var like = document.getElementById("like2");
+  like.addEventListener("click", function () {
     document.getElementById("like2").classList.toggle("like--active");
   });
 
-  var likeF = document.getElementById("like3");
-  likeF.addEventListener("click", function () {
+  var like = document.getElementById("like3");
+  like.addEventListener("click", function () {
     document.getElementById("like3").classList.toggle("like--active");
   });
 
-  var likeF = document.getElementById("like4");
-  likeF.addEventListener("click", function () {
+  var like = document.getElementById("like4");
+  like.addEventListener("click", function () {
     document.getElementById("like4").classList.toggle("like--active");
   });
 
-  var likeF = document.getElementById("like5");
-  likeF.addEventListener("click", function () {
+  var like = document.getElementById("like5");
+  like.addEventListener("click", function () {
     document.getElementById("like5").classList.toggle("like--active");
   });
 
-  var likeF = document.getElementById("like6");
-  likeF.addEventListener("click", function () {
+  var like = document.getElementById("like6");
+  like.addEventListener("click", function () {
     document.getElementById("like6").classList.toggle("like--active");
   });
+ 
+  var bookmark = document.getElementById("bookmark1");
+  bookmark.addEventListener("click", function () {
+    document.getElementById("bookmark1").classList.toggle("bookmarks--active");
+  });
 
-  $(".phone").mask("+7 (000) 000-00-00");
+  var bookmark = document.getElementById("bookmark2");
+  bookmark.addEventListener("click", function () {
+    document.getElementById("bookmark2").classList.toggle("bookmarks--active");
+  });
 
+  var bookmark = document.getElementById("bookmark3");
+  bookmark.addEventListener("click", function () {
+    document.getElementById("bookmark3").classList.toggle("bookmarks--active");
+  });
+
+  //модальное окно
   var modalButton = $("[data-toggle=modal]");
   var closeModalButton = $(".modal__close");
   modalButton.on("click", openModal);
   closeModalButton.on("click", closeModal);
 
+  //закрывается при нажатии esc modal-dialog
+  $(document).keydown(function (event) {
+    if (event.keyCode == 27) {
+      $(".modal__overlay").removeClass("modal__overlay--visible");
+      $(".modal__dialog").removeClass("modal__dialog--visible");
+    }
+  });
+
+  //закрытие при клике мимо окна
+  $(document).click( function(event){
+    if ( $(event.target).closest('.modal').length ) 
+    $('.modal').fadeOut();
+  });
+
+  //открывается при нажатии на кнопку
   function openModal() {
     var modalOverlay = $(".modal__overlay");
     var modalDialog = $(".modal__dialog");
@@ -101,6 +172,7 @@ $(document).ready(function () {
     modalDialog.addClass("modal__dialog--visible");
   }
 
+  //закрывается при нажатии на кнопку Закрыть
   function closeModal(event) {
     event.preventDefault();
     var modalOverlay = $(".modal__overlay");
@@ -111,6 +183,7 @@ $(document).ready(function () {
     $(".body").css("overflow", "auto");
   }
 
+  //валидация для формы
   $(".form").each(function () {
     $(this).validate({
       errorClass: "invalid",
@@ -131,18 +204,29 @@ $(document).ready(function () {
       },
     })})
 
+    $(".phone").mask("+7 (000) 000-00-00");
+
+  //валидация для подписки
+  $(".subscription-form").each(function () {
+    $(this).validate({
+      errorClass: "invalid-form",
+      messages: {
+        email: {
+          required: "Укажите почту",
+          email: "Неверный формат",
+        },
+      },
+    })})
+
+    //мобильное меню
     var menuButton = document.querySelector(".menu-button");
     menuButton.addEventListener("click", function () {
       document
         .querySelector(".navbar-bottom")
         .classList.toggle("navbar-bottom--visible");
     });
-  
-   /* var menuButton = document.querySelector(".button-mobile");
-    menuButton.addEventListener("click", function () {
-      document.querySelector("body").classList.toggle("body--hidden");
-    });*/
 
+    //слайдер с темами книг
     var swiper = new Swiper(".theme-swiper", {
       direction: "horizontal",
       spaceBetween: 26,
@@ -181,7 +265,8 @@ $(document).ready(function () {
         },
       },
     });
-    
+
+    //слайдер с неизданными книгами
     var swiper = new Swiper(".unreleased-swiper", {
       direction: "horizontal",
       slidesPerView: 5,
